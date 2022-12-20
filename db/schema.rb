6b944c99661756de61bf4ff1523a397c447e8e22
ppp <ppp.3ppj@gmail.com>
2022-12-20 20:43:07 +0700

    add time on devise migration

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_20_130851) do
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
